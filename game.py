bee0e0e73bb254a80182aa8ad9020cb3839ebf11
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-03-29 23:39:57 +0530

    Cleaned code up, improved documentation, made it less ambiguous

--- game.py
+++ game.py
@@ -10,36 +10,43 @@ class Graph:
     """ Data Members of the Pseudo-Data Type Graph """
     vertices = 0
     edges = 0
-    graph = [[]]  # A 2-D Matrix representation of a Graph
+    adjacencyMatrix = [[]]  # A 2-D Matrix representation of a Graph
 
     """ Parameterized Constructor of the Graph Class """
-    
     def __init__(self, vertices, edges):
         self.vertices = vertices
         self.edges = edges
-        self.graph = [[0 for j in range(vertices)] for i in range(vertices)]  # Initializing the entire graph with 0s
+        """ Initializing the entire graph with 0s """
+        self.adjacencyMatrix = [[0 for j in range(vertices)] for i in range(vertices)]
 
+    """ Definition of a function that adds an edge between two nodes on the adjacency matrix """
     def addEdge(self, emergentNode, terminalNode):
-        self.graph[emergentNode - 1][terminalNode - 1] = 1
-        self.graph[terminalNode - 1][emergentNode - 1] = 1
+        self.adjacencyMatrix[emergentNode - 1][terminalNode - 1] = 1
+        self.adjacencyMatrix[terminalNode - 1][emergentNode - 1] = 1
 
+    """ Definition of a function that accepts the edges of a graph and calls the addEdge function """
     def acceptGraph(self):
         for i in range(self.edges):
             emergentNode = int(input("Emergent Node: "))
             terminalNode = int(input("Terminal Node: "))
             self.addEdge(emergentNode, terminalNode)
 
+    """ Function that simply prints the entire Adjacency Matrix onto the console """
     def printAdjacencyMatrix(self):
-        print(f"{self.graph}")
+        print(f"{self.adjacencyMatrix}")
 
+    """ Definition of a function that uses matplotlib and networkx to visualize a graph """
     def visualizeGraph(self):
         for i in range(self.vertices):
             for j in range(self.vertices):
-                if self.graph[i][j] == 1:
+                if self.adjacencyMatrix[i][j] == 1:
                     visualGraph.add_edge(i, j)
 
         nx.draw(visualGraph)
         mpl.show()
+
+
+""" Driver Code """
 
 
 def main():
