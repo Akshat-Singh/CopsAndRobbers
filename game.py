fdd1a73e0e470cce3d1f6d9b17fd341f5e1dbc38
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-04-11 17:12:08 +0530

    Managed to create a series of sprites of Nodes in an effective manner using a list. Made a basic representation of nodes

--- game.py
+++ game.py
@@ -5,20 +5,35 @@ import gameGraph
 pygame.init()  # Initialize pygame module
 surface = pygame.Surface((100, 100))
 
-
 """ Game Window Attributes """
-screenSize = (800, 720)
+screenSize = (1500, 1000)
 screen = pygame.display.set_mode(screenSize)
 pygame.display.set_caption("Cops and Robbers")
-screen.fill([255, 255, 0])
+screen.fill([255, 255, 255])
 
 """ Sprite Attributes """
 
 # COP ATTRIBUTES #
 cop = pygame.sprite.Sprite()
 cop.image = pygame.transform.scale(pygame.image.load("sprites/cop.png").convert_alpha(), (50, 50))
-cop.rect = cop.image.get_rect()
+cop.rect = cop.image.get_rect(center=(75, 75))
 screen.blit(cop.image, cop.rect)
+
+# GRAPH ATTRIBUTES #
+nodeVector = [pygame.sprite.Sprite() for i in range(10)]
+counter = 1
+for node in nodeVector:
+    node.image = pygame.transform.scale(pygame.image.load("sprites/node.png").convert_alpha(), (75, 75))
+    node.rect = node.image.get_rect(center=(counter * 75, counter * 75))
+    screen.blit(node.image, node.rect)
+    counter = counter + 1
+
+"""
+graphNode = pygame.sprite.Sprite()
+graphNode.image = pygame.transform.scale(pygame.image.load("sprites/node.png").convert_alpha(), (50, 50))
+graphNode.rect = graphNode.image.get_rect()
+screen.blit(graphNode.image, graphNode.rect)
+"""
 
 
 def gameplay(gameRunning):
